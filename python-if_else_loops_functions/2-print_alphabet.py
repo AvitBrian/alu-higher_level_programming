989461f1942410cc64378979fd3773703b372a65
AvitBrian <b.mugisha2@alustudent.com>
2022-10-18 15:11:28 -0700

    fixed minor bugs

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for each in range(97,123):
+for each in range(97, 123):
     print("{}".format(chr(each)), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for each in range(97,123):
-    if chr(each) != 'e' and chr(each) != 'q':                                                                   
-        print("{}".format(chr(each)), end="") 
+for each in range(97, 123):
+    if chr(each) != 'e' and chr(each) != 'q':
+        print("{}".format(chr(each)), end="")
