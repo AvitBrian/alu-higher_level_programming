7eaea69126998f67364527895e94ca1ec948ed01
AvitBrian <b.mugisha2@alustudent.com>
2022-10-18 14:20:18 -0700

    initial commit

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for each in range(97,123):
+    print(f"{chr(each)}", end="")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for each in range(97,123):
+    if chr(each) != 'e' and chr(each) != 'q':                                                                   
+        print(f"{chr(each)}", end="") 
