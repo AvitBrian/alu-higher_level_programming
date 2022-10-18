36b8891dabc157b7fd7a1734984613545806c4da
AvitBrian <b.mugisha2@alustudent.com>
2022-10-18 14:50:26 -0700

    fixed minor bugs

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for each in range(97,123):
-    print(f"{chr(each)}", end="")
+    print("{}".format(chr(each)), end="")
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(0, 100):
     if number == 99:
-        print(f"{number}")
+        print("{}".format(number))
     else:
-        print(f"{number:02}", end=", ")    
+        print("{:02}".format(number), end=", ")    
