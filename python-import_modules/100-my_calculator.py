30566cd6410f6455c9f7a7ec134104e87a71d24b
AvitBrian <b.mugisha2@alustudent.com>
2022-10-19 09:02:05 -0700

    bonus tasks

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     operators = {"+": add,
-            "-": sub,
-            "*": mul,
-            "/": div,}
+                 "-": sub,
+                 "*": mul,
+                 "/": div, 
+                }
     if operator not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)    
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[operator](a, b)))
