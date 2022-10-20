37bf46aa48a8af049bf0503ca399ab5f6db568ca
AvitBrian <b.mugisha2@alustudent.com>
2022-10-20 05:02:04 -0700

    bonus task

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,18 +2,17 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    operator = sys.argv[2]
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     operators = {"+": add,
                  "-": sub,
                  "*": mul,
-                 "/": div, 
-                }
+                 "/": div,
+                 }
     if sys.argv[2] not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[operator](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
