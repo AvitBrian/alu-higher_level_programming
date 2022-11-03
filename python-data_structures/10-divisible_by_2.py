7de4b0cad0a2a89e1de9043ae57f5998c3641789
AvitBrian <b.mugisha2@alustudent.com>
2022-11-03 08:17:28 -0700

    final commit

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return None
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
     maxInt = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > maxInt:
