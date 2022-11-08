0935aefe1c310b6c52d596f13ebd984ceaa3f326
AvitBrian <b.mugisha2@alustudent.com>
2022-11-08 05:37:12 -0800

    Initial commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        x =10
-        my_list = [1, 2, 3, 4, 5]
-        for each in range(x):
+    ret = 0
+    for each in range(x):
+        try:
             print(f"{my_list[each]}", end="")
-        print()
-    except:
-        print()
+            ret += 1
+        except:
+            break
+    print()
+    return (ret)
