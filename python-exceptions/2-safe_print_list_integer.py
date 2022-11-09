91ec576a4e525e8d2e832da2218f1835dc576acd
AvitBrian <b.mugisha2@alustudent.com>
2022-11-09 05:48:15 -0800

    2nd task

---
+++ python-exceptions/2-safe_print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for y in range(x+1):
+        try:
+            if str(my_list[y]).isdigit():
+                print("{:d}".format(my_list[y]))         
+        except IndexError:
+            print("List out  of range")
