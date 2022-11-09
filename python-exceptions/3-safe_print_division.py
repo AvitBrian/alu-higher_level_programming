98a3275fa468a8e8feba34d1fdca9772df32c0f7
AvitBrian <b.mugisha2@alustudent.com>
2022-11-09 06:26:18 -0800

    2nd task

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except(TypeError, ValueError):
+    except(TypeError, ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
