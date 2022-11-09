50921a280ce1419706d3fb2c96676f31bd393839
AvitBrian <b.mugisha2@alustudent.com>
2022-11-09 06:04:14 -0800

    2nd task

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
-    for y in range(x):
+    for y in range(0, x):
         try:
-            if str(my_list[y]).isdigit():
-                print("{:d}".format(my_list[y]), end ="")
-                ret += 1
+            print("{:d}".format(my_list[y]), end ="")
+            ret += 1
+        except:
+            continue
     print()
     return ret
