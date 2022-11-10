f7c498f1069be30e5c3376e7a89a11929f2b54c6
AvitBrian <b.mugisha2@alustudent.com>
2022-11-10 07:22:07 -0800

    new commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ the class square with size and size validation"""
 
+
 class Square:
     """ defining the class square. """
-
 
     def __init__(self, size=0):
         """defining size and validation"""
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
