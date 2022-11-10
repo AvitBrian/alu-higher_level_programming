df5001ed99dde1c474f04d2cc778a8d639e4ba07
AvitBrian <b.mugisha2@alustudent.com>
2022-11-10 07:24:09 -0800

    new commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """define a class square"""
 
+
 class Square:
     """ Representing the square"""
 
     def __init__(self, size=0):
-        """ constructor initialization 
-        and sise definition"""
-
+        """ constructor initialization and sise definition"""
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
         elif size < 0:
             raise ValueError("Size must be >=0")
         self.__size = size
 
-
     def area(self):
         """ returning calculated area of the square"""
-        return (self.__size * self.__size)
+        return (self.__size ** 2)
