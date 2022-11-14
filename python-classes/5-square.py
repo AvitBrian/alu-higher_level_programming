bf25ea510d044861b2ca6f5ca236261d9d9e4ad0
AvitBrian <b.mugisha2@alustudent.com>
2022-11-14 01:13:17 -0800

    again?

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,28 +4,30 @@
 
 
 class Square:
-    """ Square class definition"""
-
+    """Class definition. """
     def __init__(self, size=0):
-        """ constructor initialization """
-
+        """constructor initialized"""
         self.__size = size
-
     @property
     def size(self):
-        """ getting size of the square """
+        """getting the size of the square"""
         return self.__size
-
     @size.setter
     def size(self, value):
-        """ setting the size of the square """
-        if not isinstance(value, int):
+        """setting the size of the square"""
+        if not value isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
     def area(self):
-        """Return the area of the square"""
-
-        return (self.__size * self.__size)
+        """Return calculated area of the square"""
+        return (self.__size ** 2)
+    def my_print(self):
+        """print the square with the "#" character. """
+        square_area = area()
+        for _ in range(0, square_area):
+            [print("#", end="") for _ in range(0, square_area)
+                    print()
+                if square_area == 0:
+            print()
