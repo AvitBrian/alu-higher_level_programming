eafa0dacaf59b5fbee99e754719a4edb66304baf
AvitBrian <b.mugisha2@alustudent.com>
2022-11-17 08:25:03 -0800

    new commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,23 +4,25 @@
 
 class Rectangle:
     """defining the class"""
+
     def __init__(self, width=0, height=0):
     """ constructor initialization"""
-        self.__width = width
-        self.__height = height
+
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """ getting the width of the rectangle"""
-        self.__width = width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """setting the width of the rectangle"""
         if not isinstance(value, int):
-            raise TypeError("with must be an integer")
-        elif width < 0:
-            raise ValueError("with must be >= 0")
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -33,7 +35,7 @@ class Rectangle:
         """setting the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif width < 0:
+        if width < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
