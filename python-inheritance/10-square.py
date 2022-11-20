cafa378a0cb2216f24acd205c45452dcd58680e9
AvitBrian <b.mugisha2@alustudent.com>
2022-11-20 10:18:51 -0800

    final commit

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,12 +5,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
     """Represent a square."""
 
-
     def __init__(self, size):
-
         """Initialize a new square. """
         self.integer_validator("size", size)
         super().__init__(size, size)
