ad049a9e29c6f191f86e8bdd17fa51abd9f79f10
AvitBrian <b.mugisha2@alustudent.com>
2022-11-21 03:22:46 -0800

    final commit

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ define a pascal's function """
 
+
 def pascal_triangle(n):
     """ with size (n) represent the triangle """
 
@@ -12,7 +13,7 @@ def pascal_triangle(n):
         tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i +1])
+            tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
     return triangles
