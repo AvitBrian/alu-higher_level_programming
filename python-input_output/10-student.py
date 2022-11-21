252570ccd56076d9d4c34e030acae3581430d714
AvitBrian <b.mugisha2@alustudent.com>
2022-11-21 03:32:37 -0800

    final commit

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,12 +6,13 @@ class Student:
     """ has student attributes """
     def __init__(self, first_name, last_name, age):
         """ initialization function """
-        self.fname = first_name
-        self.lname = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ return dict representation of Studen, filteredt"""
+
+        """return filtered dict representation of Student"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
