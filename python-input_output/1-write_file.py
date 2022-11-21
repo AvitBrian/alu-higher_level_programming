b82814de05e77d93e145ef84f5ceb938d3da3534
AvitBrian <b.mugisha2@alustudent.com>
2022-11-21 00:59:40 -0800

    initial commit

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes from the string provided in the string arg"""
+    
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+    
+
