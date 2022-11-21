f695f8f59e3e26c6fda655e3a1ce1e5ee1e115d5
AvitBrian <b.mugisha2@alustudent.com>
2022-11-21 01:02:13 -0800

    initial commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function to read a file"""
 
+
 def read_file(filename=""):
     """ reads a file"""
     with open(filename, encoding="utf-8") as f:
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,8 +4,5 @@
 
 def write_file(filename="", text=""):
     """writes from the string provided in the string arg"""
-    
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-    
-
