18dca4f233bcebaf3ad4b53d73bc395f0b9abdb9
AvitBrian <b.mugisha2@alustudent.com>
2022-11-21 02:50:16 -0800

    committed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,9 +5,9 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = \
-            __import__('7-save_to_json_file').save_to_json_file
+            __import__('5-save_to_json_file').save_to_json_file
     load_to_json_file = \
-            __import__('8-load_to_json_file').load_to_json_file
+            __import__('6-load_to_json_file').load_to_json_file
 
     try:
         content = load_from_json_file("add_item.json")
