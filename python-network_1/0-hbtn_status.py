74e9c538015d63e9d1673a67e0d962387bd56e3b
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 09:17:44 -0800

    initial commit

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetch https://intranet.hbtn.io/status."""
+import urllib.request
+
+if __name__ == "__main__":
+    url = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' sends a reques to url and displays a certain variable '''
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(url) as response:
+        print(dict(response.headers).get("X-Request-ID"))
