e08ea003c862f153e4ad1a9b05d03540e858276d
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 12:59:48 -0800

    committed

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email" : sys.argv[2]}
+    value = {"email": sys.argv[2]}
 
     response = requests.post(url, data=value)
     print(response.text)
--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     q = "" if len(sys.argv) == 1 else sys.argv[1]
-    data = {"q" : letter}
+    data = {"q": q}
 
     response = requests.post("http://0.0.0.0:5000/search_user", data=data)
     try:
