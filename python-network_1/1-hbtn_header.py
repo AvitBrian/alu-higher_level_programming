ded9fd2f1f3d3ae68c8ad0f1e88ad0f35e588f3d
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 09:46:49 -0800

    initial commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-ID"))
+        print(dict(response.headers).get("X-Request-Id"))
