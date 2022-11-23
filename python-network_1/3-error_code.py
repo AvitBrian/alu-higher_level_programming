812cd4ebcc610438b70041ac9c573081f6f4993c
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 10:45:07 -0800

    committing

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''sends a request to a url, displays the response and catches errors'''
+import sys
+import urllib.request
+import urllib.error
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
