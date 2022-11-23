5d0247ece907ba73bba218f0276af9bd64eba02b
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 13:35:07 -0800

    final commit

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,6 +5,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.agrv[2])
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     response = requests.get("https://api.github.com/user", auth=auth)
     print(response.json().get("id"))
