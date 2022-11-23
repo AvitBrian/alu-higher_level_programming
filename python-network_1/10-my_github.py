6c4c359f9b8738771995237774aeefad5d70cbaf
AvitBrian <b.mugisha2@alustudent.com>
2022-11-23 13:34:17 -0800

    final commit

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' uses the github api to display github id '''
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.agrv[2])
+    response = requests.get("https://api.github.com/user", auth=auth)
+    print(response.json().get("id"))
