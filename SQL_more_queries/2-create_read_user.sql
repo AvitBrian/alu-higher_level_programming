7ba8b27b94cb843b00b265717723dfb4400398ff
AvitBrian <b.mugisha2@alustudent.com>
2022-11-25 10:57:29 -0800

    sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
 -- createa new database and user
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
-CREATE USER IF NOT EXIST 'user_0d_2'@'localhost'
-IDENTIFIED BY user_0d_2_pwd;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
-IDENTIFIED BY user_0d_2_pwd;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
