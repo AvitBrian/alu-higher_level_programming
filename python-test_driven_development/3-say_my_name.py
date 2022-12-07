5d6896fb80f520602fc825e53f639d8aa0118577
AvitBrian <b.mugisha2@alustudent.com>
2022-12-07 15:07:59 -0800

    init

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """ prints out a sentence with assigned args"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
