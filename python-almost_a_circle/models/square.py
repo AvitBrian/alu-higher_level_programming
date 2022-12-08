8f1b2c44bf1c6fd8b2b981408cd8299b22929832
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 13:14:17 -0800

    comitted

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" the square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ represents a square """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializaation"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ get/set size """
+        return self.height
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
