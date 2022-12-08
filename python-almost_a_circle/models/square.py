af8a6f58b5d035d6136398be69995834e81a51b5
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 13:23:17 -0800

    comitted

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,7 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ represents a square """
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         """initializaation"""
         super().__init__(size, size, x, y, id)
@@ -14,7 +14,13 @@ class Square(Rectangle):
     def size(self):
         """ get/set size """
         return self.height
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __str__(self):
+        """print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
