3676776f5e5d5b8940f1fee4472f39854c18be91
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 08:27:37 -0800

    commited

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" the Rectangle class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ represents a rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ get/set width """
+        return self.__width
+
+    @width.setter
+    def with(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get/set height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ get/set x coordinates """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value <= 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+    
+    @property
+    def y(self):
+        """ get/set y coordinates """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
