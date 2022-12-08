a30cacf382079cea68d74047457723aac7e5d47e
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 15:31:08 -0800

    committed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -64,3 +64,35 @@ class Base:
                 return [cls.create(**dictionary) for dictionary in list_dicts]
         except IOError:
             return []
+    def save_to_file_csv(cls, list_objs):
+        """ save to csv file """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+
+                w = csv.Dictwriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    w.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ loads from csv file """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([key, int(value)] for key, value in dictionary.items())
+                              for dictionary in list_dicts]
+                return [cls.create(**dictionary) for dictionary in list_dicts]
+        except IOError:
+            return []
