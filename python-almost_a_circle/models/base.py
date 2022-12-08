1b642949f6fab2f44e5ae20e8d68e5f63fc8ce4d
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 13:48:39 -0800

    comitted

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ json dump string """
         if list_dictionaries is None or list_dictionaries == []:
