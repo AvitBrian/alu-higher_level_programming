7b3a83e9bd8e035ae99b30f56839fdb7e06b3e3c
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 13:46:36 -0800

    comitted

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ the class Base """
+import json
 
 
 class Base:
@@ -13,3 +14,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        new = json.dumps(list_dictionaries)
+        return new
