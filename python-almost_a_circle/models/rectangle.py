ea66f895ce77933aa15032a9ff927a37381bdceb
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 12:51:23 -0800

    init

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -132,4 +132,3 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-
