5b422a8cb02cc6c168c6ed24c5d29e4da14165f9
AvitBrian <b.mugisha2@alustudent.com>
2022-12-08 07:34:26 -0800

    committed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ the class Base """
 
+
 class Base:
     """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ init function"""    
-        if id != None:
+        """ init function"""
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
