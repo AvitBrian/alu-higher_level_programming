7453b4bffd33d5c5c4fbb245d78edb128a2bf185
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 23:04:28 -0800

    updates

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -7,10 +7,9 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True
-                    )
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
