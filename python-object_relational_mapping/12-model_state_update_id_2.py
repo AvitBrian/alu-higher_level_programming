c7fa3764b88477e85fa3eec70ec5f882f8f5df20
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 23:51:45 -0800

    12-task

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" updates the database """
+"""updates the database"""
+
+import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import sys
 
 
 if __name__ == "__main__":
