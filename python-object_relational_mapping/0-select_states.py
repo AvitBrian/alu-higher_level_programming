3d8344073c3451ec92b069b3bfeea619d3d6811a
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 16:26:08 -0800

    initial commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ lists all the states from a given database"""
 import sys
-from MySQLdb
+import MySQLdb
 
 if __name__ == "__main__":
     """ setting the file as a script"""
     with MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.agrv[3],
+        db=sys.argv[3],
         host='localhost',
         port=3306
         ) as s:
