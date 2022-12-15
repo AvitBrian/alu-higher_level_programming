c2a754f968cd4e26a81656a07791dedc836fff7c
AvitBrian <b.mugisha2@alustudent.com>
2022-12-15 13:56:02 -0800

    final commit

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    result = session.query(State, City) \
-        .filter(State.id == City.state_id).order_by(City.id).all()
-    for row in result:
-        print("{}: ({}) {}".format(
-            state[0].__dict__['name'],
-            state[1].__dict__['id'],
-            state[1].__dict__['name']))
-    session.close()
+    with Session(engine) as session:
+        result = session.query(State, City) \
+            .filter(State.id == City.state_id).order_by(City.id).all()
+
+        for row in result:
+            print("{}: ({}) {}".format(
+                row[0].__dict__['name'],
+                row[1].__dict__['id'],
+                row[1].__dict__['name']))
