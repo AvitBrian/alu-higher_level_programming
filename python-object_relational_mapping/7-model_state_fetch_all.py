c4737b42d35d8b8d5156ad16a86271f47b5ba332
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 19:29:31 -0800

    more work

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ lists all state objects"""
 import sys
-from model_state Base, State
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
