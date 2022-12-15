d441ec7ea5a721a86c7b0fe7861522988408ad77
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 22:50:03 -0800

    updates

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" list state object from input"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True
+                    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)   
+    new_state = State(name="Louisiana").add()
+    session.commit()
+    print("{}".format(state.id))
+    session.close()
