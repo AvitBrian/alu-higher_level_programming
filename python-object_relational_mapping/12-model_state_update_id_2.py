60880850e2d156a4d5c64d0782091961601fd10e
AvitBrian <b.mugisha2@alustudent.com>
2022-12-15 11:12:38 -0800

    12-task

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    with session() as session:
-        Base.metadata.create_all(engine)
-        state = session.query(State).filter(State.id == 2).first()
-        state.name = "New Mexico"
-        session.commit()
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
