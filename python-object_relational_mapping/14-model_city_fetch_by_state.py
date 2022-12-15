16d49a57cd1b2596a44b90c997073a7fcce28574
AvitBrian <b.mugisha2@alustudent.com>
2022-12-15 15:25:25 -0800

    final commit?

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""prints all city objects """
-from sqlalchemy.orm import session
-from sqlalchemy import create_engine
-from model_city import City
-from model_state import State, Base
+"""lists city objects in the database """
 import sys
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
 if __name__ == "__main__":
-    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(user, passwd, db),
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    with Session(engine)as session:
-        for city, state in session.query(City, State) \
-                    .filter(City.state_id == State.id) \
-                    .order_by(City.id).all():
-            print(f"{state.name}: ({city.id}) {city.name}")
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
