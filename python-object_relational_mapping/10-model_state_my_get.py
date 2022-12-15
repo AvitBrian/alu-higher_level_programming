fa9416dbf947a7005814db48192a9621a48d4562
AvitBrian <b.mugisha2@alustudent.com>
2022-12-15 09:21:43 -0800

    updates

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,14 +12,10 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-
-    state_name = sys.argv[4]
-
-    state = session.query(State).filter(State.name == state_name).first()
-    if state is None:
-        print("Not found")
-    else:
-        print(f'{state.id}')
-
-    session.close()
+    with Session(engine) as session:
+        state_name = sys.argv[4]
+        state = session.query(State).filter(State.name == state_name).first()
+        if state is None:
+            print("Not found")
+        else:
+            print(f'{state.id}')
