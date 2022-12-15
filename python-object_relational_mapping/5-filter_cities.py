1a05d114d56c01eea9e0be264a03703b54984eae
AvitBrian <b.mugisha2@alustudent.com>
2022-12-14 18:28:42 -0800

    5_task

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" no injections this time! """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    with MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host='localhost',
+            port=3306,
+    )as conn:
+        cur = conn.cursor()
+        query = """ SELECT c.name
+                    FROM states as s, cities as c
+                    WHERE s.name = '{:.d}'
+                    ORDER BY c.id ASC
+                """
+        cur.execute(query.format(sys.argv[4])
+        cities = cur.fetchall()
+        for city in cities:
+            print(city)
+        cur.close()
