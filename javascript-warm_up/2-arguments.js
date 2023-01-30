e2a339e9cc8b1bfa5a0915ebc91dcfadc3f34269
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-01-30 20:57:14 +0000

    committed 😁😁

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const [,, ...args] = process.argv;
 
 if (args.length === 0) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (args.length < 2 && args.length > 0) {
   console.log('Argument found');
 } else if (args.length > 1) {
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const [,, ...args] = process.argv;
 
-if (args === 0) {
+if (args == 0) {
   console.log('No argument');
 } else { console.log(...args); }
