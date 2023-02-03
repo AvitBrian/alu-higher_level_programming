084a7a27386fe5727811fa4f52d13bb1c40eaa96
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-02-03 11:34:15 +0000

    committed 😁😁

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,9 +4,9 @@ const newSquare = require('./5-square.js');
 module.exports = class Square extends newSquare {
   charPrint (c) {
     if (c === undefined) {
-        for (let m = 0; m < this.height; m++) {
-            console.log('X'.repeat(this.width));
-          }
+      for (let m = 0; m < this.height; m++) {
+        console.log('X'.repeat(this.width));
+      }
     } else {
       for (let m = 0; m < this.height; m++) {
         console.log(c.repeat(this.width));
